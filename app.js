a1bbcceea8a90d00d9abf2ea93a6d7ae55bf1015
Joel Pablo <joel.pablo@digitalmonkstudio.com>
2014-05-03 05:44:22 +0800

    first commit yo

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var routes = require('./routes');
+var path = require('path');
+
+var app = express();
+
+// environment setup
+var port = process.env.PORT || 9000;
+app.use(express.static(__dirname + '/public')); // static files location
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// var router = express.Router();
+
+app.get('/', routes.index);
+
+app.listen(port);
+console.log(' ----- Started app at port ' + port);
+
+
