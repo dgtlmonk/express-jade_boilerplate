f0f8286f0b2d6d40a634784c27474e5df1b34b06
Joel Pablo <joel.pablo@digitalmonkstudio.com>
2014-05-11 05:54:27 +0800

    loaded via jsonp

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var routes = require('./routes');
 var path = require('path');
 
+
 var app = express();
 
 // environment setup
@@ -9,6 +10,7 @@ var port = process.env.PORT || 9000;
 app.use(express.static(__dirname + '/public')); // static files location
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(require('connect-assets')());
 
 // var router = express.Router();
 
